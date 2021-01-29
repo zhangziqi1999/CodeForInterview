6e346391c0dfe96f8904a1b4790f43008fcbafb2
solo <1327698867@qq.com>
2021-01-29 11:01:05 +0800

    update

--- src/main/java/javaBase/Singleton.java
+++ src/main/java/javaBase/Singleton.java
@@ -1,9 +1,9 @@
 package javaBase;
 
 public class Singleton {
-    static volatile Singleton SINGLETON = null;
+    private static volatile Singleton SINGLETON = null;
 
-    static Singleton get(){
+    public static Singleton get(){
         if(SINGLETON == null){
             synchronized (Singleton.class){
                 if (SINGLETON == null){
